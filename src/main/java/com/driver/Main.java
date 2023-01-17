1c60829b7ac99007e11f53e7c6dd40bb7917d995
Mohammad-Mubarak <beyourbestversion3@gmail.com>
2023-01-17 16:12:07 +0530

    DONE

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,25 @@
 package com.driver;
 
 public class Main {
+     public static void main(String[] args){
+         Product p=new Product();
+         p.product(5,6);
+         p.product(5,7,9);
+         p.product(4.3,6.5);
+     }
+  static  class  Product{
+      public int product(int x, int y) {
+          return x*y;
+      }
 
+      public int product(int x, int y, int z) {
+          return x*y*z;
+      }
+
+      public double product(double x, double y) {
+          return  x*y;
+      }
+
+
+    }
 }
